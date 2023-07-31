660fc9c1ba1d37551a1c78e4fe18486db36cea3a
Uwe Inthanet <info@inthanet.com>
2023-07-31 17:18:00 +0700

    Removed some empty lines

--- itcs/dbconnect.php
+++ itcs/dbconnect.php
@@ -5,8 +5,6 @@ define('DB_USERNAME', 'bizdir_adm');//ENTER YOUR DB USERNAME
 define('DB_PASSWORD', 'p01c2kr_2561');//ENTER YOUR DB PASSWORD
 define('DB_NAME', 'bizdir_dbase');//ENTER YOUR DB NAME
 
-
-
 # Connection to the database. #
 $conn = mysqli_connect(DB_HOSTNAME, DB_USERNAME, DB_PASSWORD)
 or die('Unable to connect to MySQL');
