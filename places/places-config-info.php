f2d1181dbe8539e51d7c2023acc880c2818d39c0
Uwe Inthanet <info@inthanet.com>
2023-08-07 04:55:38 +0700

    Deleted old language file assignment

--- places/places-config-info.php
+++ places/places-config-info.php
@@ -16,27 +16,6 @@ if(file_exists('../admin/config/config.php'))
 
 $footer_row = getAllFooter(); //Fetch Footer Data
 
-if($footer_row['admin_language']== 1) {
-    if (file_exists('../admin/config/all_texts_english.php')) {
-        include('../admin/config/all_texts_english.php');
-    }
-}elseif($footer_row['admin_language']== 2) {
-    if (file_exists('../admin/config/all_texts_arabic.php')) {
-        include('../admin/config/all_texts_arabic.php');
-    }
-}elseif($footer_row['admin_language']== 3) {
-    if (file_exists('../admin/config/all_texts_french.php')) {
-        include('../admin/config/all_texts_french.php');
-    }
-}elseif($footer_row['admin_language']== 4) {
-    if (file_exists('../admin/config/all_texts_spanish.php')) {
-        include('../admin/config/all_texts_spanish.php');
-    }
-}else{
-    if (file_exists('../admin/config/all_texts_english.php')) {
-        include('../admin/config/all_texts_english.php');
-    }
-}
 
 $current_page = basename($_SERVER['PHP_SELF']);
 
